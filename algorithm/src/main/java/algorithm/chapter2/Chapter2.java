1c9d05baa8a40cd0c300e26b5ab177127d2dd2a1
chenl <131602111@qq.com>
2018-12-09 21:42:20 +0800

    。。

--- algorithm/src/main/java/algorithm/chapter2/Chapter2.java
+++ algorithm/src/main/java/algorithm/chapter2/Chapter2.java
@@ -17,22 +17,6 @@ public class Chapter2 {
         return nums;
     }
 
-    public static int[] insertSortDesc(int[] nums, String sortType) {
-        if (nums.length <= 1)
-            return nums;
-        for (int i = 1; i < nums.length; i++) {
-            int key = nums[i];
-            int j = i - 1;
-            while (j >= 0 && nums[j] < key) {
-                nums[j + 1] = nums[j];
-                j = j - 1;
-            }
-
-            nums[j + 1] = key;
-        }
-        return nums;
-    }
-
     public static void main(String[] args) {
         int[] nums = {1, 5, 2, 4, 6, 10, 7, 8};
         int[] ret = insertSort(nums, "asc");
